6bd78cf0ffbd213ebaa08c2e58cc025ac51a8194
Matthias Steiner <m@kjpjm.de>
2021-03-11 22:58:40 +0100

    add playground page

---
+++ pages/playground.js
@@ -0,0 +1,10 @@
+import style from "../styles/playground.module.css";
+import Loader from "../components/Loader";
+
+export default function playground(props) {
+  return (
+    <div className={style.container}>
+      <Loader></Loader>
+    </div>
+  );
+}
